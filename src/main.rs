523c376dd746830583eb5023c37c89205f51d9e5
taylor.fish <contact@taylor.fish>
2026-01-19 18:03:29 -0800

    Minimize further

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,26 @@
+//! This code compiles on nightly (2026-01-20) but fails to compile on stable (1.92).
 use enum_dispatch::enum_dispatch;
 
-#[enum_dispatch(ExampleEnum)]
-trait Example {
-    fn value(&self) -> i32;
-}
-
-#[derive(Debug)]
-struct Alpha;
-
-impl Example for Alpha {
-    fn value(&self) -> i32 {
-        1
-    }
-}
-
-#[derive(Debug)]
-struct Beta;
-
-impl Example for Beta {
-    fn value(&self) -> i32 {
-        2
-    }
-}
+struct A;
+impl Example for A {}
 
 macro_rules! define_enum {
-    ($name:ident, $($type:ident),* $(,)?) => {
+    ($type:ident) => {
         #[enum_dispatch]
-        #[derive(Debug)]
-        pub enum $name {
-            $(
-                $type,
-            )*
+        enum ExampleEnum {
+            $type
         }
     };
 }
 
-define_enum!(ExampleEnum, Alpha, Beta);
+#[enum_dispatch(ExampleEnum)]
+trait Example {
+    fn foo(&self) {}
+}
+
+// If you move this macro invocation above the declaration of `trait Example`, it compiles.
+define_enum!(A);
 
 fn main() {
-    let value = ExampleEnum::Alpha(Alpha).value();
-    println!("{value}");
+    ExampleEnum::A(A).foo();
 }
